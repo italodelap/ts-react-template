c6385f7637ae17e26706f4f2ca01f5c9753e6d67
Italo De la Peña <italo.delapena@utdt.edu>
2023-06-05 10:28:59 -0300

    feat(*): ✨ cleanup the project and configure Husky

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ export function App() {
 
 	return (
 		<div className="App">
-			<h3>⚡⚛️ Vite React Best Practices Template (by Codely)</h3>
+			<h3>⚡ ⚛️ Typescript & React Template</h3>
 			<h2>Current users</h2>
 
 			{users.map((user) => (
